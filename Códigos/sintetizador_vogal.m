39fb984cdcffe0af2c5fa1432688f184e93266ef
souzaitor <heitpsouza@gmail.com>
2022-03-28 12:47:39 -0300

    Update Files

--- Códigos/sintetizador_vogal.m
+++ Códigos/sintetizador_vogal.m
@@ -1,41 +1,42 @@
 % Murillo Rodrigo Petrucelli Homem
-% CÃ³digo exemplo para sintetizar vogais
+% Código exemplo para sintetizar vogais
 
-% Tabela com valores de formantes para a sÃ­ntese de vogais
-%                                           Primeiro formante        |       Segundo formante         |       Vogal
-% FrequÃªncias                                   800                   |                   1200                  |         /a/
-%                                                       460                   |                   1800                  |         /e/ (aberto)
-%                                                       570                   |                   1590                  |         /e/ (fechado)
-%                                                       320                   |                   2110                  |         /i/
-%                                                       450                   |                   950                    |         /o/ (aberto)
-%                                                       560                   |                   1050                  |         /o/ (fechado)
-%                                                       320                   |                   850                    |         /u/
+% Tabela com valores de formantes para a síntese de vogais
+%
+%                                           Primeiro formante          |            Segundo formante             |        Vogal
+%                                                800                   |                   1200                  |         /a/
+%                                                460                   |                   1800                  |         /e/ (aberto)
+%                                                570                   |                   1590                  |         /e/ (fechado)
+%                                                320                   |                   2110                  |         /i/
+%                                                450                   |                   950                   |         /o/ (aberto)
+%                                                560                   |                   1050                  |         /o/ (fechado)
+%                                                320                   |                   850                   |         /u/
 
 clear all
 close all
 
-nome = input("Entre com o nome da vogal: ");  % exemplo, digite entre aspas "a.wav". NÃ£o esqueÃ§a da extensÃ£o wave
-% uma tabela com as frequÃªncias dos primeiro e segundo formantes para vogais 
-% da lingua Portuguesa estÃ¡ no inÃ­cio deste arquivo
+nome = input("Entre com o nome da vogal: ");  % Exemplo, digite entre aspas "a.wav". Não esqueça da extensão wave
+% uma tabela com as frequências dos primeiro e segundo formantes para vogais 
+% da lingua Portuguesa está no início deste arquivo
 formante_1 = input("Entre com o primeiro formante (em Hz): "); 
 formante_2 = input("Entre com o segundo formante (em Hz): ");
 
-% ParÃ¢metros fixos para o experimento
-sigma = 3;  % desvio padrÃ£o para os filtros de ressonÃ¢ncia (o aluno fica convidado a testar outros valores)
-T  = 1; % o sinal gerado Ã© de 1 segundo
-f0 = 120; % frequÃªncia fundamental do sinal de base para uma voz masculina, mais grave (em mÃ©dia)
-H = 40; % serÃ£o utilizados 40 harmÃ´nicos na sÃ­ntese da onda dente de serra
+% Parâmetros fixos para o experimento
+sigma = 3;    % desvio padrão para os filtros de ressonância (o aluno fica convidado a testar outros valores)
+T  = 1;       % o sinal gerado é de 1 segundo
+f0 = 120;     % frequência fundamental do sinal de base para uma voz masculina, mais grave (em média)
+H = 40;       % serÃ£o utilizados 40 harmÃ´nicos na sÃ­ntese da onda dente de serra
 
-% para o caso da mÃ©dia da f0 em mulheres, em torno de 220 Hz, o nÃºmero de harmÃ´nicos deve ser aproximadamente 20
-% (neste cÃ³digo, o nÃºmero de harmÃ´nicos estÃ¡ sendo ajustado para nÃ£o violar o teorema de Nyquist)
+% para o caso da média da f0 em mulheres, em torno de 220 Hz, o número de harmônicos deve ser aproximadamente 20
+% (neste código, o número de harmônicos está sendo ajustado para não violar o teorema de Nyquist)
 
-% calculando a frequÃªncia angular (sua medida Ã© rad/s ou radianos por segundo)
+% Calculando a frequência angular (sua medida é rad/s ou radianos por segundo)
 w0 = 2*pi*f0;
-delta = 0.0001;  % Cuidado! A frequÃªncia mÃ¡xima que o sinal poderÃ¡ conter Ã© de 5 kHz (teorema de Nyquist)
+delta = 0.0001;  % Cuidado! A frequência máxima que o sinal poderá conter é de 5 kHz (teorema de Nyquist)
 dw = 1/delta;
-A = 1; % amplitude da onda para referÃªncia (serÃ¡ mantida em um)
+A = 1;           % Amplitude da onda para referência (será mantida em um)
 
-% gera uma onda dente de serra
+% Gera uma onda dente de serra
 x = 0;
 for tempo = [0:delta:T-delta] 
     x = x + 1;
@@ -50,7 +51,7 @@ endfor
 sinal_padding = [sinal zeros(1,dw)];
 ft_sinal = fft(sinal_padding,dw); 
 
-% ConstrÃ³i os filtros passa-banda no domÃ­nio de Fourier (frequÃªncia) para os formantes
+% Cnstrói os filtros passa-banda no domínio de Fourier (frequência) para os formantes
 for n = [1:dw]
      filtro1(n) = exp((-1)*(((n-formante_1)^2)/(2*(sigma^2))));
      filtro2(n) = exp((-1)*(((n-formante_2)^2)/(2*(sigma^2))));
@@ -61,18 +62,18 @@ ft_sinal_filtrado_f1 =  ft_sinal .* filtro1;
 % filtra a onda dente de serra (considerando o segundo formante)
 ft_sinal_filtrado_f2 =  ft_sinal .* filtro2;
 
-% voltando para o domÃ­nio do tempo...
+% voltando para o domínio do tempo...
 sinal_filtrado_f1 = ifft(ft_sinal_filtrado_f1);
 sinal_filtrado_f2 = ifft(ft_sinal_filtrado_f2);
 sinal_filtrado =  sinal_filtrado_f1 .+ sinal_filtrado_f2;
 
-% ajusta a amplitude do sinal nas frequÃªncias desejadas (simulando o processo de ressonÃ¢ncia)
-fator_amplificacao = 10; % o aluno Ã© convidado a trabalhar com outros valores
-sinal_filtrado_amplificado = fator_amplificacao .* sinal_filtrado; % lembrando que sÃ£o nÃºmeros complexos
+% Ajusta a amplitude do sinal nas frequências desejadas (simulando o processo de ressonância)
+fator_amplificacao = 10; % o aluno é convidado a trabalhar com outros valores
+sinal_filtrado_amplificado = fator_amplificacao .* sinal_filtrado; % lembrando que são números complexos
 
 vogal_gerada = sinal .* sinal_filtrado_amplificado;
 
-% gera uma envoltÃ³ria Gaussiana
+% gera uma envoltória Gaussiana
 x = 0;
 sigma = 0.1;
 for tempo = [0:delta:T-delta] 
@@ -82,7 +83,7 @@ endfor
 
 vogal_gerada = envoltoria .* real(vogal_gerada); % agora temos valores reais
 
-% normalizando o Ã¡udio da vogal gerada entre [-1,1]
+% normalizando o áudio da vogal gerada entre [-1,1]
 vogal_gerada = vogal_gerada ./ max(vogal_gerada);
 
 hold on; 
@@ -94,4 +95,3 @@ hold off;
 
 % salva a vogal
 audiowrite (nome,vogal_gerada,dw);
-
